672f99b5170c5b4de31d45718b249d4b0d68c176
psh153 <105466406+psh153@users.noreply.github.com>
2022-07-14 22:31:31 +0900

    Add files via upload

---
+++ baekJoon/Puls2.java
@@ -0,0 +1,17 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class Puls2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+
+}
